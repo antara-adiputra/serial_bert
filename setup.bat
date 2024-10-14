b7be4bd1ad27c9583b76c9f7018415efb7a35570
antara-adiputra <antara.adiputra@gmail.com>
2024-10-14 10:01:24 +0800

    Bug fix on setup file

--- setup.bat
+++ setup.bat
@@ -4,6 +4,7 @@ title Serial-BERT (Setup)
 
 
 set DIR=%~dp0
+echo Directory Path = '%DIR%'
 
 if exist %DIR%.env\ (set ENVDIR=%DIR%.env\ & goto setup_pip)
 if exist %DIR%.venv\ (set ENVDIR=%DIR%.venv\ & goto setup_pip)
@@ -12,20 +13,22 @@ if exist %DIR%venv\ (set ENVDIR=%DIR%venv\ & goto setup_pip)
 
 
 :setup_venv
-echo Configuring python environment...
+echo Python virtual environment not exist, create a new one...
 set ENVDIR=%DIR%.venv\
 call python -m venv %DIR%.venv
 goto setup_pip
 
 
 :setup_pip
-echo Configuring python packages...
+echo Installing required packages...
 call %ENVDIR%Scripts\activate.bat & pip install -r requirements
+echo.
+echo Packages installed successfully!
 
 
 :exit_setup
 echo Finishing setup...
 echo ^@echo off > %DIR%run.bat
 echo call %ENVDIR%Scripts\activate.bat ^& python main.py ^--no-show >> %DIR%run.bat
-echo. && echo.
+echo.
 pause
--- setup.sh
+++ setup.sh
@@ -14,35 +14,34 @@ while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
         SOURCE=$DIR/$TARGET # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
     fi
 done
-echo "Executing '$SOURCE'"
+# echo "Executing '$SOURCE'"
 RDIR=$( dirname "$SOURCE" )
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 if [ "$DIR" != "$RDIR" ]; then
-    echo "DIR '$RDIR' resolves to '$DIR'"
+    echo "Directory Path = '$DIR'"
 fi
-# echo "DIR is '$DIR'"
 
 cd $DIR
 DIR_ENV="$(ls -a | grep env)"
 
 if [[ -z "$DIR_ENV" ]]; then
-    echo "Python virtual environment not exist, create a new one...\r\n"
+    echo "Python virtual environment not exist, create a new one..."
     /usr/bin/python3 -m venv .venv
+    DIR_ENV=".venv"
 fi
 
-echo "Installing required packages...\r\n"
+echo "Installing required packages..."
 source "$DIR/$DIR_ENV/bin/activate"
 pip install -r requirements
-echo "\r\nPackages installed successfully!\r\n"
+echo -e "\r\nPackages installed successfully!"
 
-echo "\r\nFinishing setup...\r\n"
+echo "Finishing setup..."
 cat > run.sh <<EOF
 #!/usr/bin/env bash
 
-set -e
 
 source "$DIR/$DIR_ENV/bin/activate"
 python $DIR/main.py
-echo "\r\nApplication has been stopped.\r\n\r\n"
+echo -e "\r\nApplication has been stopped.\r\n"
 EOF
 chmod +x run.sh
