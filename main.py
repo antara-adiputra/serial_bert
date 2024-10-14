dbe8c4c7e215ec3be4404fad376aaf3e5b7bdcd8
antara-adiputra <antara.adiputra@gmail.com>
2024-10-14 11:21:39 +0800

    Fix minor bugs

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = GUI()
 
 
 if __name__ in {"__main__", "__mp_main__"}:
+	print(f'Application run on {settings.ALLOWED_HOST}:{settings.BIND_PORT}')
 	ui.run(
 		host=settings.ALLOWED_HOST,
 		port=settings.BIND_PORT,
@@ -17,10 +18,9 @@ if __name__ in {"__main__", "__mp_main__"}:
 		favicon=settings.FAVICON_PATH,
 		dark=settings.DARK_MODE,
 		show=settings.AUTO_SHOW,
-		reload=True,
+		reload=False,
 		binding_refresh_interval=settings.BINDING_REFRESH_INTERVAL,
 		reconnect_timeout=settings.RECONNECT_TIMEOUT,
 		language=settings.LANGUAGE,
 		show_welcome_message=False
 	)
-	print(f'Application run on {settings.ALLOWED_HOST}:{settings.BIND_PORT}')
